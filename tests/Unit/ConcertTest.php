c6a4cc1d8164c50e4286b2f8cb047fba6a772544
LYB <king800116@gmail.com>
2017-11-30 22:12:32 +0800

    [concert] refactoring

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -9,23 +9,48 @@ use App\Concert;
 
 class ConcertTest extends TestCase
 {
-    use DatabaseMigrations;
-
     /** @test */
     public function can_get_formatted_date()
     {
         // Arrange
-        // Create a concert with a known date
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
 
         // Act
-        // Retrieve the formatted date
         $date = $concert->formatted_date;
 
         // Assert
-        // Verify the date is formatted as expected
         $this->assertEquals('December 1, 2016', $date);
+    }
+
+    /** @test */
+    public function can_get_formatted_start_time()
+    {
+        // Arrange
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+
+        // Act
+        $time = $concert->formatted_start_time;
+
+        // Assert
+        $this->assertEquals('5:00pm', $time);
+    }
+
+    /** @test */
+    public function can_get_ticket_price_in_dollars()
+    {
+        // Arrange
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        // Act
+        $price = $concert->ticket_price_in_dollars;
+
+        // Assert
+        $this->assertEquals(67.50, $price);
     }
 }
