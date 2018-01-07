af55a59ca9f4c876db856f7302a168ff899b0d67
LYB <king800116@gmail.com>
2018-01-07 20:36:06 +0800

    [order ticket] add validate email

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -13,13 +13,18 @@ class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     /** @test */
     public function customer_can_purchase_concert_tickets()
     {
-        $this->disableExceptionHandling();
         // Arrange
-        $paymentGateway = new FakePaymentGateway;
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
         $concert = factory(Concert::class)->create([
             'ticket_price' => 3250,
         ]);
@@ -28,16 +33,32 @@ class PurchaseTicketsTest extends TestCase
         $this->json('POST', "/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
-            'payment_token' => $paymentGateway->getValidTestToken(),
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
         ]);
 
         // Assert
         $this->assertResponseStatus(201);
 
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         $orders = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($orders);
         $this->assertEquals(3, $orders->tickets->count());
+    }
+
+    /** @test */
+    public function email_is_required_to_purchase_tickets()
+    {
+        // Arrange
+        $concert = factory(Concert::class)->create();
+
+        // Act
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
+        ]);
+
+        // Assert
+        $this->assertResponseStatus(422);
     }
 }
