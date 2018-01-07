a300a274bb136f11aa8665a51480c4e7bb2fbc25
LYB <king800116@gmail.com>
2018-01-07 19:18:06 +0800

    [TestCase] add disableExceptionHandling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
 abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -21,5 +24,24 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+
+    // Framework-supplied test case methods snipped for brevity
+    // Use this version if you're on PHP 7
+    protected function disableExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+
+            public function report(Exception $e)
+            {
+                // no-op
+            }
+
+            public function render($request, Exception $e) {
+                throw $e;
+            }
+        });
     }
 }
